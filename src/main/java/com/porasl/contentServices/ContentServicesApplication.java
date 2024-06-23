fba3fe363cd569f677d3ccd7210511383e8d7472
hamid <hamid@porasl.com>
2024-06-23 02:51:24 -0700

    first checkIn

---
+++ src/main/java/com/porasl/contentServices/ContentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.porasl.contentServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentServicesApplication.class, args);
+	}
+
+}
