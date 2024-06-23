a8bf787cefb87c3bb0c6dee7c87aa21aa3a42852
porasl <151912720+porasl@users.noreply.github.com>
2024-06-23 05:04:03 -0700

    Update AuthenticationRequest.java

--- src/main/java/com/porasl/contentservice/model/AuthenticationRequest.java
+++ src/main/java/com/porasl/contentservice/model/AuthenticationRequest.java
@@ -2,4 +2,32 @@ package com.porasl.contentservice.model;
 
 public class AuthenticationRequest {
 
+    private String username;
+    private String password;
+
+    // Default constructor for JSON Parsing
+    public AuthenticationRequest() {
+
+    }
+
+    public AuthenticationRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
