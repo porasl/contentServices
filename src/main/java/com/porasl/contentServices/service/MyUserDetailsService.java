58f547602b68a0a4c1f33189025ad9f848b2d241
porasl <151912720+porasl@users.noreply.github.com>
2024-06-23 04:06:35 -0700

    Update MyUserDetailsService.java

--- src/main/java/com/porasl/contentServices/service/MyUserDetailsService.java
+++ src/main/java/com/porasl/contentServices/service/MyUserDetailsService.java
@@ -1,5 +1,19 @@
 package com.porasl.contentServices.service;
 
-public class MyUserDetailsService {
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // For simplicity, we're using a hardcoded user. You should fetch user details from a database.
+        return new User("user", "$2a$10$DOWSDsVYk8uvBFS9M90GNeIOT54pR7oHBL/4y4UlINR4N7JgIj.4a", new ArrayList<>());
+    }
 }
