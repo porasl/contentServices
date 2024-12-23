ecadb90ac159b914e62d6f34e2e6730656e18855
porasl <151912720+porasl@users.noreply.github.com>
2024-12-23 01:48:40 -0800

    Swagger for Content Service
    Swagger for Content Services

--- src/main/java/com/porasl/contentServices/config/OpenAPIConfig.java
+++ src/main/java/com/porasl/contentServices/config/OpenAPIConfig.java
@@ -2,18 +2,24 @@ package com.porasl.contentservices.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OpenAPIConfig {
+public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                //.openapi("3.0.3")  // Specify OpenAPI version here
                 .info(new Info()
                         .title("File Upload API")
-                        .version("1.0")
-                        .description("API for uploading files"));
+                        .version("1.0.0")
+                        .description("API for uploading files")
+                        .termsOfService("http://swagger.io/terms/")
+                        .contact(new Contact().name("Support Team").email("support@example.com").url("http://example.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
