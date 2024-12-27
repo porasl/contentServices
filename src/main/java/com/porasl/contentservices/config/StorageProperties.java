f34c3db3aff910db37a548f67d0203a985cc1414
porasl <151912720+porasl@users.noreply.github.com>
2024-12-27 02:41:14 -0800

    added Storage Properties

---
+++ src/main/java/com/porasl/contentservices/config/StorageProperties.java
@@ -0,0 +1,14 @@
+package com.porasl.contentservices.config;
+
+public class StorageProperties {
+	String location ="";
+	
+   public String getLocation(){
+	   return location;
+   }
+   
+   public void setLocation(String location){
+	   this.location = location;
+   }
+
+}
