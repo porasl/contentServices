1cd46c403949dfb7ac811104e82fa805081da700
porasl <151912720+porasl@users.noreply.github.com>
2025-07-24 20:25:24 -0700

    Changed Kafka settings

--- src/main/java/com/porasl/contentservices/consumer/VideoEncodingConsumer.java
+++ src/main/java/com/porasl/contentservices/consumer/VideoEncodingConsumer.java
@@ -1,6 +1,9 @@
 package com.porasl.contentservices.consumer;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -10,20 +13,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class VideoEncodingConsumer {
 	
     @KafkaListener(topics = "video-uploads", groupId = "video-encoder")
-    public void consume(String messageJson) {
+    public void consume(String messageJson, Acknowledgment ack) {
         System.out.println("Received message: " + messageJson);
+        CompletableFuture.runAsync(() -> {
         try {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode node = mapper.readTree(messageJson);
             String filePath = node.get("videoTranscode") != null ? node.get("videoTranscode").asText(): null;
 
+           System.out.println("Encoding started for ********  : " + filePath);
            VideoTranscoder transcoder = new VideoTranscoder(filePath);
            transcoder.transcode();
            
-            System.out.println("Encoding complete for: " + filePath);
-
+            System.out.println("Encoding complete for ***** : " + filePath);
+            ack.acknowledge();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        });
+  
     }
 }
